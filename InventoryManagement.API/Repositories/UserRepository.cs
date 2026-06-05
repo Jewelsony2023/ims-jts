7db76dea2ccd565387b2006b11a9a6a8335f2c4a
jewelsony2023 <jewel.sony2023@vitstudent.ac.in>
2026-06-05 16:06:30 +0530

    Fixed bugs in backend

---
+++ InventoryManagement.API/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+using InventoryManagement.API.Data;
+using InventoryManagement.API.Interfaces.Repositories;
+using InventoryManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.API.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly InventoryDbContext _context;
+
+    public UserRepository(InventoryDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users
+            .Include(u => u.Role)
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<bool> EmailExistsAsync(string email)
+    {
+        return await _context.Users
+            .AnyAsync(u => u.Email == email);
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+}
